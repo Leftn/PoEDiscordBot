ef3343ccfdb568dd3f5a543bcb41cdd00d60e6eb
g_ <14917082+Leftn@users.noreply.github.com>
2018-05-04 13:47:52 +1000

    The beginnings of creating an embed when searching the price

--- helpers.py
+++ helpers.py
@@ -1,6 +1,7 @@
 import json
 
 import requests
+from discord.embeds import Embed
 
 import config
 
@@ -18,5 +19,14 @@ def titlecase(tuple):
 def pricecheck(item, league):
     data = {"league":league, "name":item}
     headers = {"Content-Type":"application/json"}
+    # The server is an API of my own making, I havn't bothered writing documentation/creating a domain name yet
+    # The server is in pre-pre-alpha and only this part of the site works
     data = requests.post("http://45.76.116.155/api/v1/get", data=json.dumps(data), headers=headers).json()
     return data
+
+def create_embed_pricing(data):
+    embed = Embed(colour=0x4f1608)
+    if data.get("type") == "map":
+        embed.add_field(name="Chaos", value=data.get("chaos"))
+        embed.add_field(name="Exalted", value=data.get("exalted"))
+    return embed

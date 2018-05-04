5706409b2bfe22d86b18943cd86c61210f50c576
Alfred <alfred.lui1@gmail.com>
2018-05-04 20:53:51 +1000

    Added new setting for browser in config

--- config.py
+++ config.py
@@ -2,6 +2,7 @@ browser_height = 2500
 browser_width = 2500
 excluded_titlecase_words = ["of", "and", "the", "to", "at", "for"]
 chromedriver = ""
+browser = ""
 client_id = ""
 token_secret =""
 leagues = ["Bestiary", "Bestiary Hardcore", "Hardcore", "Standard", "Flashback", "Flashback Hardcore"]

3e222762a823303a6541e635db3188afeb6a9ae7
g_ <14917082+Leftn@users.noreply.github.com>
2018-05-16 09:27:55 +1000

    Fixed flasks missing the legacy field in the embed

--- cogs/pricechecker.py
+++ cogs/pricechecker.py
@@ -132,7 +132,12 @@ class PriceChecker():
         elif item_type == "map":
             embed.add_field(name="Tier", value=data.get("tier"))
             embed.add_field(name="Atlas", value=data.get("atlas"))
-        if item_type != "currency":
+        elif item_type == "flask":
+            if data.get("legacy") == 0:
+                embed.add_field(name="Legacy", value="No")
+            else:
+                embed.add_field(name="Legacy", value="Yes")
+        if data.get("image"):
             embed.set_image(url=data.get("image"))
         return embed
 
